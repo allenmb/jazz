ee765052a1358f946c05db56425511cf2409af07
Allen Black <allen.m.black@gmail.com>
2019-02-14 23:54:59 -0700

    The website is now searchable for all Jazz players Statistics for the last 50 games the player played are also displayed I still need to format it nicely and probably add some pictures so far so good black out time for bed

--- script.js
+++ script.js
@@ -6,57 +6,102 @@ document.getElementById("player_submit").addEventListener("click", function (eve
     console.log(value);
 
     /* Get Current Weather */
-
-    const url = "https://www.balldontlie.io/api/v1/players?q=" + value;
+    let player_id = 0;
+    let jazzman = true;
+    const url = "https://www.balldontlie.io/api/v1/players?search=" + value;
     fetch(url)
         .then(function (response) {
             return response.json();
         }).then(function (json) {
             console.log(json);
             let results = "";
-            /*results += '<h2>Weather in ' + json.name + "</h2>";
-            for (let i = 0; i < json.weather.length; i++) {
-                results += '<img class="wthr-img" src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+            // Find the player id
+            for (let i = 0; i < json.data.length; i++) {
+                if (json.data[i].team.id === 29) {
+                    player_id = json.data[i].id;
+                    results += "<h2>" + json.data[i].first_name + " " + json.data[i].last_name + "</h2>";
+                    if (json.data[i].position != "") {
+                        results += "<p>Position: " + json.data[i].position + " on the greatest team in the NBA</p>";
+                    }
+                    break;
+                } else if (i === json.data.length - 1) {
+                    results = "<h2>This website only supports Jazz players</h2><br><h2> Begone, Heathen</h2><br>";
+                    jazzman = false;
+                    break;
+                }
             }
-            results += '<h2>' + json.main.temp + " &deg;F</h2>"
-            results += "<p>"
-            results += "<p>Press " + Number((json.main.pressure) / 1013.25 * 770 / 25.4).toFixed(1) + " in</p>";
-            results += "<p>Wind " + json.wind.speed + " mph  <img src='/images/arrow.png' height='15' style=transform:rotate(" + json.wind.deg + "deg);></p>";
-            document.getElementById("weatherResults").innerHTML = results;*/
+            document.getElementById("player_summary").innerHTML = results;
+        }).then(function () {
+            console.log(player_id);
+            const url2 = "https://www.balldontlie.io/api/v1/stats?&seasons=2018&player_ids[]=" + player_id + "&per_page=50";
+            fetch(url2)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (json) {
+                    console.log(json);
+                    let stats = ""
+                    if (jazzman) {
+                        stats = "<h2>Last 50 Games</h2>";
+                        //calculate stats
+                        let total_games = 0;
+                        let total_pts = 0;
+                        let total_reb = 0;
+                        let total_ast = 0;
+                        let total_fgm = 0;
+                        let total_fga = 0;
+                        let total_fg3m = 0;
+                        let total_fg3a = 0;
+                        let total_ftm = 0;
+                        let total_fta = 0;
+                        let total_blk = 0;
+                        let total_stl = 0;
+                        let total_turnover = 0;
+                        for (let i = 0; i < json.data.length; i++) {
+                            total_games++;
+                            total_pts += json.data[i].pts;
+                            total_reb += json.data[i].reb;
+                            total_ast += json.data[i].ast;
+                            total_fgm += json.data[i].fgm;
+                            total_fga += json.data[i].fga;
+                            total_fg3m += json.data[i].fg3m;
+                            total_fg3a += json.data[i].fg3a;
+                            total_ftm += json.data[i].ftm;
+                            total_fta += json.data[i].fta;
+                            total_blk += json.data[i].blk;
+                            total_stl += json.data[i].stl;
+                            total_turnover += json.data[i].turnover;
+                        }
+                        let ppg = total_pts / total_games;
+                        let rpg = total_reb / total_games;
+                        let apg = total_ast / total_games;
+                        let bpg = total_blk / total_games;
+                        let spg = total_stl / total_games;
+                        let att = total_ast / total_turnover;
+                        let ftp = total_ftm / total_fta * 100;
+                        let fgp = total_fgm / total_fga * 100;
+                        let fg3p = 0
+                        if (total_fg3a != 0) {
+                            fg3p = total_fg3m / total_fg3a * 100;
+                        }
+                        // Report Stats
+                        stats += "<p> PTS  " + total_pts + "</p>";
+                        stats += "<p> REB  " + total_reb + "</p>";
+                        stats += "<p> AST  " + total_ast + "</p>";
+                        stats += "<p> BLK  " + total_blk + "</p>";
+                        stats += "<p> STL  " + total_stl + "</p>";
+                        stats += "<p> TOV  " + total_turnover + "</p>";
+                        stats += "<p> PPG  " + ppg.toFixed(1) + "</p>";
+                        stats += "<p> RPG  " + rpg.toFixed(1) + "</p>";
+                        stats += "<p> APG  " + apg.toFixed(1) + "</p>";
+                        stats += "<p> BPG  " + bpg.toFixed(1) + "</p>";
+                        stats += "<p> SPG  " + spg.toFixed(1) + "</p>";
+                        stats += "<p> A:TO " + att.toFixed(1) + "</p>";
+                        stats += "<p> FT%  " + ftp.toFixed(1) + "</p>";
+                        stats += "<p> FG%  " + fgp.toFixed(1) + "</p>";
+                        stats += "<p> FG3% " + fg3p.toFixed(1) + "</p>";
+                    }
+                    document.getElementById("player_stats").innerHTML = stats;
+                });
         });
 
-    /* Get 5 Day Forecast */
-/*
-    const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=2ee99ec06c8e0364cf1110a30e9bd7f0";
-    fetch(url2)
-        .then(function (response) {
-            return response.json();
-        }).then(function (json) {
-            console.log(json);
-            let forecast = "";
-            let counter = 0;
-            for (let i = 0; i < json.list.length; i++) {
-                let x = moment(json.list[i].dt) - 1549670400;
-                if (x % 86400 === 0 || i === 0) {
-                    counter++;
-                    if (counter != 1) {
-                        forecast += "</div>";
-                    }
-                    if (i != (json.list.length - 1)) {
-                        forecast += "<div class='span2'><h3>" + moment(json.list[i].dt_txt).format('MMMM Do') + "</h3>";
-                    }
-                }
-                forecast += "<h5>" + moment(json.list[i].dt_txt).format('h a') + "</h5>";
-                forecast += '<img class="wthr-img" src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
-                forecast += "<p>Temp " + json.list[i].main.temp + " &deg;F</p>";
-                forecast += "<p>Press " + Number((json.list[i].main.pressure) / 1013.25 * 770 / 25.4).toFixed(1) + " in</p>";
-                forecast += "<p>Wind " + json.list[i].wind.speed + " mph  <img src='/images/arrow.png' height='15' style=transform:rotate(" + json.list[i].wind.deg + "deg);></p>";
-                if (i === (json.list.length - 1)) {
-                    forecast += "</div>";
-                }
-                x = 0;
-            }
-            console.log(forecast);
-            document.getElementById("forecastResults").innerHTML = forecast;
-        });*/
 });
